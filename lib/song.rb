b0906eb51419984033395034fe7819e6d06e2c67
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-04-26 00:11:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
-  
+
   def self.count
     @@count
   end
@@ -9,15 +9,15 @@ class Song
   @@genres = []
 
   def self.genres
-    @@genres.uniq 
-  end 
+    @@genres.uniq
+  end
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @@artists << artist
     @genre = genre
-    @@genres << genre 
+    @@genres << genre
     @@count += 1
   end
 end
