e8f8c92678910a06f8b21a1c6dbc8a7b273d40d9
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-04-26 00:26:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,12 +12,18 @@ class Song
     @@genres.uniq
   end
 
-  def new(name, artist, genre)
-  end
-
   @@artists = []
 
   def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end 
+    artist_count 
 end
 
   def self.artists
